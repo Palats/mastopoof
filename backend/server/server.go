dcaa7311c0104374d866f09142b9ff0bcd1028b5
Pierre Palatin <pierre@palatin.fr>
2024-01-24 20:37:45 +0100

    Remove unused RPC endpoints

--- backend/server/server.go
+++ backend/server/server.go
@@ -8,7 +8,6 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/Palats/mastopoof/backend/storage"
-	"github.com/golang/glog"
 
 	pb "github.com/Palats/mastopoof/proto/gen/mastopoof"
 )
@@ -33,75 +32,6 @@ func (s *Server) Ping(ctx context.Context, req *connect.Request[pb.PingRequest])
 		Msg: fmt.Sprintf("Got: %v", req.Msg),
 	})
 	return resp, nil
-}
-
-func (s *Server) InitialStatuses(ctx context.Context, req *connect.Request[pb.InitialStatusesRequest]) (*connect.Response[pb.InitialStatusesResponse], error) {
-	resp := &pb.InitialStatusesResponse{}
-
-	lid := int64(1)
-
-	opened, err := s.st.Opened(ctx, lid)
-	if err != nil {
-		return nil, err
-	}
-	for _, openedStatus := range opened {
-		raw, err := json.Marshal(openedStatus.Status)
-		if err != nil {
-			return nil, err
-		}
-		resp.Items = append(resp.Items, &pb.Item{
-			Status:   &pb.MastodonStatus{Content: string(raw)},
-			Position: openedStatus.Position,
-		})
-	}
-
-	lstate, err := s.st.ListingState(ctx, s.st.DB, lid)
-	if err != nil {
-		return nil, err
-	}
-	resp.LastRead = lstate.LastRead
-
-	return connect.NewResponse(resp), nil
-}
-
-func (s *Server) GetStatus(ctx context.Context, req *connect.Request[pb.GetStatusRequest]) (*connect.Response[pb.GetStatusResponse], error) {
-	lid := int64(1)
-	ost, err := s.st.StatusAt(ctx, lid, req.Msg.Position)
-	if err != nil {
-		return nil, err
-	}
-
-	if ost == nil {
-		// Status was not found - is it time to pick another one?
-		lastPosition, err := s.st.LastPosition(ctx, lid, s.st.DB)
-		if err != nil {
-			return nil, err
-		}
-		if req.Msg.Position > lastPosition+1 {
-			// The requested position is beyond even the next status we could
-			// fetch, so bail out.
-			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("no status at position %d", req.Msg.Position))
-		}
-		ost, err = s.st.PickNext(ctx, lid)
-		if err != nil {
-			return nil, err
-		}
-		glog.Infof("picked next status at position %d", ost.Position)
-		if ost.Position != req.Msg.Position {
-			return nil, fmt.Errorf("TODO: race condition when picking up next entry")
-		}
-	}
-
-	resp := &pb.GetStatusResponse{}
-	raw, err := json.Marshal(ost.Status)
-	if err != nil {
-		return nil, err
-	}
-	resp.Item = &pb.Item{
-		Status:   &pb.MastodonStatus{Content: string(raw)},
-		Position: ost.Position,
-	}
-	return connect.NewResponse(resp), nil
 }
 
 func (s *Server) Fetch(ctx context.Context, req *connect.Request[pb.FetchRequest]) (*connect.Response[pb.FetchResponse], error) {
