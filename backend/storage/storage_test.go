8874750677bf4b8217719de1fa11e3567d53a4f3
Pierre Palatin <pierre@palatin.fr>
2024-03-23 22:42:10 +0100

    Extract structure from the DB schema before comparing.

--- backend/storage/storage_test.go
+++ backend/storage/storage_test.go
@@ -7,9 +7,40 @@ import (
 	"testing"
 )
 
-func canonicalSchema(ctx context.Context, db *sql.DB) (string, error) {
-	resp := ""
+type SchemaDB struct {
+	Tables []*SchemaTable
+}
 
+// Linear produces a string version of the whole schema suitable for line-based diffing.
+func (s *SchemaDB) Linear() string {
+	resp := ""
+	for _, table := range s.Tables {
+		for _, c := range table.Columns {
+			resp += fmt.Sprintf("table=%s, cid=%d, name=%s, type=%s, notnull=%v, dflt_value=(%t, %q), pk=%d\n",
+				c.TableName, c.CID, c.Name, c.Type, c.NotNull, c.DefaultValue.Valid, c.DefaultValue.String, c.PrimaryKey,
+			)
+		}
+	}
+	return resp
+}
+
+type SchemaTable struct {
+	TableName string
+	Columns   []*SchemaColumn
+}
+
+type SchemaColumn struct {
+	TableName string
+
+	CID          int64
+	Name         string
+	Type         string
+	NotNull      string
+	DefaultValue sql.NullString
+	PrimaryKey   int64
+}
+
+func canonicalSchema(ctx context.Context, db *sql.DB) (*SchemaDB, error) {
 	// Find all tables.
 	tablesRows, err := db.QueryContext(ctx, `
 		SELECT
@@ -24,25 +55,24 @@ func canonicalSchema(ctx context.Context, db *sql.DB) (string, error) {
 			name
 	;`)
 	if err != nil {
-		return "", fmt.Errorf("unable to obtain schema info: %w", err)
+		return nil, fmt.Errorf("unable to obtain schema info: %w", err)
 	}
 
 	tableNames := []string{}
 	for tablesRows.Next() {
 		var tableName string
 		if err := tablesRows.Scan(&tableName); err != nil {
-			return "", fmt.Errorf("unable to extract table name: %w", err)
+			return nil, fmt.Errorf("unable to extract table name: %w", err)
 		}
 		tableNames = append(tableNames, tableName)
 	}
 	if err := tablesRows.Err(); err != nil {
-		return "", fmt.Errorf("failed to list tables: %w", err)
+		return nil, fmt.Errorf("failed to list tables: %w", err)
 	}
 
 	// And for each tables, find all the columns.
+	schemaDB := &SchemaDB{}
 	for _, tableName := range tableNames {
-		resp += fmt.Sprintf("# %s\n", tableName)
-
 		query := fmt.Sprintf(`
 			SELECT
 				cid,
@@ -59,31 +89,30 @@ func canonicalSchema(ctx context.Context, db *sql.DB) (string, error) {
 
 		columnsRows, err := db.QueryContext(ctx, query, tableName)
 		if err != nil {
-			return "", fmt.Errorf("unable to get columns info for %s: %w", tableName, err)
+			return nil, fmt.Errorf("unable to get columns info for %s: %w", tableName, err)
 		}
+
+		schemaTable := &SchemaTable{
+			TableName: tableName,
+		}
+		schemaDB.Tables = append(schemaDB.Tables, schemaTable)
 
 		// Go over the columns of the table.
 		for columnsRows.Next() {
-			var colCID int64
-			var colName string
-			var colType string
-			var colNotNull string
-			var colDefaultValue sql.NullString
-			var colPrimaryKey int64
-			if err := columnsRows.Scan(&colCID, &colName, &colType, &colNotNull, &colDefaultValue, &colPrimaryKey); err != nil {
-				return "", fmt.Errorf("unable to extract column info in table %s: %w", tableName, err)
+			schemaColumn := &SchemaColumn{
+				TableName: tableName,
+			}
+			schemaTable.Columns = append(schemaTable.Columns, schemaColumn)
+			if err := columnsRows.Scan(&schemaColumn.CID, &schemaColumn.Name, &schemaColumn.Type, &schemaColumn.NotNull, &schemaColumn.DefaultValue, &schemaColumn.PrimaryKey); err != nil {
+				return nil, fmt.Errorf("unable to extract column info in table %s: %w", tableName, err)
 			}
 
-			resp += fmt.Sprintf("table=%s, cid=%d, name=%s, type=%s, notnull=%v, dflt_value=(%t, %q), pk=%d\n",
-				tableName, colCID, colName, colType, colNotNull, colDefaultValue.Valid, colDefaultValue.String, colPrimaryKey,
-			)
 		}
 		if err := columnsRows.Err(); err != nil {
-			return "", fmt.Errorf("failed to list columns for %s: %w", tableName, err)
+			return nil, fmt.Errorf("failed to list columns for %s: %w", tableName, err)
 		}
-
 	}
-	return resp, nil
+	return schemaDB, nil
 }
 
 const refSchema = `
@@ -125,6 +154,20 @@ const refSchema = `
 func TestDBCreate(t *testing.T) {
 	ctx := context.Background()
 
+	// Create a reference database schema.
+	dbRef, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := dbRef.ExecContext(ctx, refSchema); err != nil {
+		t.Fatal(err)
+	}
+	refSch, err := canonicalSchema(ctx, dbRef)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a mastopoof database.
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		t.Fatal(err)
@@ -138,20 +181,7 @@ func TestDBCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	dbRef, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if _, err := dbRef.ExecContext(ctx, refSchema); err != nil {
-		t.Fatal(err)
-	}
-
-	refSch, err := canonicalSchema(ctx, dbRef)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if sch != refSch {
-		t.Fatal(sch, "\n\n\n", refSch)
+	if got, want := sch.Linear(), refSch.Linear(); got != want {
+		t.Errorf("=== Got:\n%s\n=== Want:\n%s\n", got, want)
 	}
 }
