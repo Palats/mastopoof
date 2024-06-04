dfe8b188ab2cdfe428d12a548f4ecbd4d57797fe
Pierre Palatin <pierre@palatin.fr>
2024-06-04 18:03:00 +0200

    Rework CLI commands definition.
    Cobra flag parsing is still very odd and flags sometimes do not get
    detected when before the command.

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -23,45 +22,70 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var _ = spew.Sdump("")
+var ()
 
-var (
-	port        = flag.Int("port", 8079, "Port to listen on for the 'serve' command")
-	userID      = flag.Int64("uid", 0, "User ID to use for commands. With 'serve', will auto login that user.")
-	streamID    = flag.Int64("stream_id", 0, "Stream to use")
-	dbFilename  = flag.String("db", "", "SQLite file")
-	showAccount = flag.Bool("show_account", false, "Query and show account state from Mastodon server")
-	selfURL     = flag.String("self_url", "", "URL to use for authentication redirection on the frontend. When empty, uses out-of-band auth.")
-	inviteCode  = flag.String("invite_code", "", "If not empty, users can only be created by providing this code.")
-	testData    = flag.String("testdata", "localtestdata", "Directory with backend testdata, for testserve")
-	doFix       = flag.Bool("fix", false, "If set, update streamstate based on computed value.")
-	insecure    = flag.Bool("insecure", false, "If true, mark cookies as insecure, allowing serving without https")
-)
+func FlagPort(fs *pflag.FlagSet) *int {
+	return fs.Int("port", 8079, "Port to listen on for the 'serve' command")
+}
+
+func FlagUserID(fs *pflag.FlagSet) *storage.UID {
+	return (*storage.UID)(fs.Int64("uid", 0, "User ID to use for commands. With 'serve', will auto login that user."))
+}
+
+func FlagStreamID(fs *pflag.FlagSet) *storage.StID {
+	return (*storage.StID)(fs.Int64("stream_id", 0, "Stream to use"))
+}
+
+func FlagDBFilename(fs *pflag.FlagSet) *string {
+	return fs.String("db", "", "SQLite file")
+}
+
+func FlagSelfURL(fs *pflag.FlagSet) *string {
+	return fs.String("self_url", "", "URL to use for authentication redirection on the frontend. When empty, uses out-of-band auth.")
+}
+func FlagInviteCode(fs *pflag.FlagSet) *string {
+	return fs.String("invite_code", "", "If not empty, users can only be created by providing this code.")
+}
+func FlagInsecure(fs *pflag.FlagSet) *bool {
+	return fs.Bool("insecure", false, "If true, mark cookies as insecure, allowing serving without https")
+}
+
+/*func FlagPort(af *AllFlags, fs *pflag.FlagSet) {
+	fs.IntVar(&af.port, "port", 8079, "Port to listen on for the 'serve' command")
+}
+
+func FlagDBFilename(af *AllFlags, fs *pflag.FlagSet) {
+	fs.StringVar(&af.dbFilename, "db", "", "SQLite file")
+}
+
+type DeclFlag func(*AllFlags, *pflag.FlagSet)
+
+type AllFlags struct {
+	port       int
+	dbFilename string
+}
+
+func (f *AllFlags) Register(fs *pflag.FlagSet, decls ...DeclFlag) {
+	for _, d := range decls {
+		d(f, fs)
+	}
+}
+
+var allFlags = AllFlags{}
+
+func NewCommand(flagDecls []DeclFlag, cmd *cobra.Command) *cobra.Command {
+	allFlags.Register(cmd.PersistentFlags(), flagDecls...)
+	return cmd
+}*/
 
 const appMastodonScopes = "read write push"
 
-func getStorage(ctx context.Context, filename string) (*storage.Storage, error) {
-	if filename == "" {
-		return nil, fmt.Errorf("missing database filename; try specifying --db <filename>")
+func getStreamID(ctx context.Context, st *storage.Storage, streamID storage.StID, userID storage.UID) (storage.StID, error) {
+	if streamID != 0 {
+		return streamID, nil
 	}
-
-	st, err := storage.NewStorage(ctx, filename, *selfURL, appMastodonScopes)
-	if err != nil {
-		return nil, err
-	}
-	return st, nil
-}
-
-func getUserID(_ context.Context, _ *storage.Storage) (storage.UID, error) {
-	return storage.UID(*userID), nil
-}
-
-func getStreamID(ctx context.Context, st *storage.Storage) (storage.StID, error) {
-	if *streamID != 0 {
-		return storage.StID(*streamID), nil
-	}
-	if *userID != 0 {
-		userState, err := st.UserState(ctx, nil, storage.UID(*userID))
+	if userID != 0 {
+		userState, err := st.UserState(ctx, nil, userID)
 		if err != nil {
 			return 0, err
 		}
@@ -70,7 +94,7 @@ func getStreamID(ctx context.Context, st *storage.Storage) (storage.StID, error)
 	return 0, errors.New("no streamID / user ID specified")
 }
 
-func getMux(st *storage.Storage, autoLogin storage.UID) (*http.ServeMux, error) {
+func getMux(st *storage.Storage, autoLogin storage.UID, inviteCode string, insecure bool, selfURL string) (*http.ServeMux, error) {
 	mux := http.NewServeMux()
 
 	// Serve frontend content (html, js, etc.).
@@ -82,243 +106,327 @@ func getMux(st *storage.Storage, autoLogin storage.UID) (*http.ServeMux, error) 
 
 	// Run the backend RPC server.
 	sessionManager := server.NewSessionManager(st)
-	if *insecure {
+	if insecure {
 		sessionManager.Cookie.Secure = false
 	}
-	s := server.New(st, sessionManager, *inviteCode, autoLogin, *selfURL, appMastodonScopes)
+	s := server.New(st, sessionManager, inviteCode, autoLogin, selfURL, appMastodonScopes)
 	s.RegisterOn(mux)
 	return mux, nil
 }
 
-func run(ctx context.Context) error {
-	var rootCmd = &cobra.Command{
-		Use:   "mastopoof",
-		Short: "Mastopoof is a Mastodon client",
-		Long:  `More about Mastopoof`,
-	}
-
-	rootCmd.AddCommand(&cobra.Command{
+func cmdUsers() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "users",
 		Short: "List users",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
+		return cmds.CmdUsers(ctx, st)
+	}
+	return c
+}
 
-			return cmds.CmdUsers(ctx, st)
-		},
-	})
-
-	rootCmd.AddCommand(&cobra.Command{
+func cmdClearApp() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "clear-app",
 		Short: "Remove app registrations from local DB, forcing Mastopoof to recreate them when needed.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
+		return st.ClearApp(ctx)
+	}
+	return c
+}
 
-			return st.ClearApp(ctx)
-		},
-	})
-	rootCmd.AddCommand(&cobra.Command{
+func cmdClearStream() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "clear-stream",
 		Short: "Remove all statuses from the stream, as if nothing was ever looked at.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	streamID := FlagStreamID(c.PersistentFlags())
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			stid, err := getStreamID(ctx, st)
-			if err != nil {
-				return err
-			}
-			glog.Infof("using stream ID %d", stid)
+		stid, err := getStreamID(ctx, st, *streamID, *userID)
+		if err != nil {
+			return err
+		}
+		glog.Infof("using stream ID %d", stid)
+		return st.ClearStream(ctx, stid)
+	}
+	return c
+}
 
-			return st.ClearStream(ctx, stid)
-		},
-	})
-	rootCmd.AddCommand(&cobra.Command{
+func cmdClearPoolStream() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "clear-pool-stream",
 		Short: "Remove all statuses from the pool and stream, as if nothing had ever been fetched.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			uid, err := getUserID(ctx, st)
-			if err != nil {
-				return err
-			}
-			return st.ClearPoolAndStream(ctx, uid)
-		},
-	})
+		return st.ClearPoolAndStream(ctx, *userID)
+	}
+	return c
+}
 
-	rootCmd.AddCommand(&cobra.Command{
+func cmdMe() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "me",
 		Short: "Get information about one's own account.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	showAccount := c.PersistentFlags().Bool("show_account", false, "Query and show account state from Mastodon server")
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			uid, err := getUserID(ctx, st)
-			if err != nil {
-				return err
-			}
+		return cmds.CmdMe(ctx, st, *userID, *showAccount)
+	}
+	return c
+}
 
-			return cmds.CmdMe(ctx, st, uid, *showAccount)
-		},
-	})
-
-	rootCmd.AddCommand(&cobra.Command{
+func cmdServe() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "serve",
 		Short: "Run mastopoof backend server",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	port := FlagPort(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	inviteCode := FlagInviteCode(c.PersistentFlags())
+	insecure := FlagInsecure(c.PersistentFlags())
 
-			uid, err := getUserID(ctx, st)
-			if err != nil {
-				return err
-			}
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			mux, err := getMux(st, uid)
-			if err != nil {
-				return err
-			}
-			addr := fmt.Sprintf(":%d", *port)
-			fmt.Printf("Listening on %s...\n", addr)
-			return http.ListenAndServe(addr, h2c.NewHandler(mux, &http2.Server{}))
-		},
-	})
+		mux, err := getMux(st, *userID, *inviteCode, *insecure, *selfURL)
+		if err != nil {
+			return err
+		}
+		addr := fmt.Sprintf(":%d", *port)
+		fmt.Printf("Listening on %s...\n", addr)
+		return http.ListenAndServe(addr, h2c.NewHandler(mux, &http2.Server{}))
+	}
+	return c
+}
 
-	rootCmd.AddCommand(&cobra.Command{
+func cmdTestServe() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "testserve",
 		Short: "Run mastopoof backend server with a fake mastodon server",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, ":memory:")
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	port := FlagPort(c.PersistentFlags())
+	inviteCode := FlagInviteCode(c.PersistentFlags())
+	insecure := FlagInsecure(c.PersistentFlags())
+	testData := c.PersistentFlags().String("testdata", "localtestdata", "Directory with backend testdata, for testserve")
 
-			serverAddr := fmt.Sprintf("http://localhost:%d", *port)
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, ":memory:", *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			_, err = st.CreateAppRegState(ctx, nil, serverAddr)
-			if err != nil {
-				return fmt.Errorf("unable to create server state: %w", err)
-			}
+		serverAddr := fmt.Sprintf("http://localhost:%d", *port)
 
-			userState, _, _, err := st.CreateUser(ctx, nil, serverAddr, "1234", "testuser1")
-			if err != nil {
-				return fmt.Errorf("unable to create testuser: %w", err)
-			}
+		_, err = st.CreateAppRegState(ctx, nil, serverAddr)
+		if err != nil {
+			return fmt.Errorf("unable to create server state: %w", err)
+		}
 
-			mux, err := getMux(st, userState.UID)
-			if err != nil {
-				return err
-			}
+		userState, _, _, err := st.CreateUser(ctx, nil, serverAddr, "1234", "testuser1")
+		if err != nil {
+			return fmt.Errorf("unable to create testuser: %w", err)
+		}
 
-			return cmds.CmdTestServe(ctx, st, mux, *port, *testData)
-		},
-	})
+		mux, err := getMux(st, userState.UID, *inviteCode, *insecure, *selfURL)
+		if err != nil {
+			return err
+		}
 
-	rootCmd.AddCommand(&cobra.Command{
+		return cmds.CmdTestServe(ctx, st, mux, *port, *testData)
+	}
+	return c
+}
+
+func cmdPickNext() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "pick-next",
 		Short: "Add a status to the stream",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	streamID := FlagStreamID(c.PersistentFlags())
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			stid, err := getStreamID(ctx, st)
-			if err != nil {
-				return err
-			}
+		stid, err := getStreamID(ctx, st, *streamID, *userID)
+		if err != nil {
+			return err
+		}
 
-			return cmds.CmdPickNext(ctx, st, stid)
-		},
-	})
-	rootCmd.AddCommand(&cobra.Command{
+		return cmds.CmdPickNext(ctx, st, stid)
+	}
+	return c
+}
+
+func cmdSetRead() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "set-read",
 		Short: "Set the already-read pointer",
 		Args:  cobra.MaximumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	streamID := FlagStreamID(c.PersistentFlags())
+
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
+
+		stid, err := getStreamID(ctx, st, *streamID, *userID)
+		if err != nil {
+			return err
+		}
+
+		position := int64(-1)
+		if len(args) > 0 {
+			position, err = strconv.ParseInt(args[0], 10, 64)
 			if err != nil {
 				return err
 			}
-			defer st.Close()
+		}
+		return cmds.CmdSetRead(ctx, st, stid, position)
+	}
+	return c
+}
 
-			stid, err := getStreamID(ctx, st)
-			if err != nil {
-				return err
-			}
-
-			position := int64(-1)
-			if len(args) > 0 {
-				position, err = strconv.ParseInt(args[0], 10, 64)
-				if err != nil {
-					return err
-				}
-			}
-			return cmds.CmdSetRead(ctx, st, stid, position)
-		},
-	})
-	rootCmd.AddCommand(&cobra.Command{
+func CmdCheckStreamState() *cobra.Command {
+	c := &cobra.Command{
 		Use:   "check-stream-state",
 		Short: "Compare stream state values to its theoritical values.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			st, err := getStorage(ctx, *dbFilename)
-			if err != nil {
-				return err
-			}
-			defer st.Close()
+	}
+	dbFilename := FlagDBFilename(c.PersistentFlags())
+	c.MarkFlagRequired("db")
+	selfURL := FlagSelfURL(c.PersistentFlags())
+	userID := FlagUserID(c.PersistentFlags())
+	streamID := FlagStreamID(c.PersistentFlags())
+	doFix := c.PersistentFlags().Bool("fix", false, "If set, update streamstate based on computed value.")
 
-			stid, err := getStreamID(ctx, st)
-			if err != nil {
-				return err
-			}
+	c.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		st, err := storage.NewStorage(ctx, *dbFilename, *selfURL, appMastodonScopes)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
 
-			return cmds.CmdCheckStreamState(ctx, st, stid, *doFix)
-		},
-	})
+		stid, err := getStreamID(ctx, st, *streamID, *userID)
+		if err != nil {
+			return err
+		}
 
-	return rootCmd.ExecuteContext(ctx)
+		return cmds.CmdCheckStreamState(ctx, st, stid, *doFix)
+	}
+	return c
 }
 
 func main() {
 	ctx := context.Background()
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.Parse()
 	fmt.Println("Mastopoof")
 
-	err := run(ctx)
-	if err != nil {
+	rootCmd := &cobra.Command{
+		Use:   "mastopoof",
+		Short: "Mastopoof is a Mastodon client",
+		Long:  `More about Mastopoof`,
+	}
+
+	rootCmd.AddCommand(cmdUsers())
+	rootCmd.AddCommand(cmdClearApp())
+	rootCmd.AddCommand(cmdClearStream())
+	rootCmd.AddCommand(cmdClearPoolStream())
+	rootCmd.AddCommand(cmdMe())
+	rootCmd.AddCommand(cmdServe())
+	rootCmd.AddCommand(cmdTestServe())
+	rootCmd.AddCommand(cmdPickNext())
+	rootCmd.AddCommand(cmdSetRead())
+	rootCmd.AddCommand(CmdCheckStreamState())
+
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		glog.Exit(err)
 	}
 }
