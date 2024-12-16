d643721a971b491474a646cd14e1447af26f6792
Pierre Palatin <pierre@palatin.fr>
2024-12-16 22:26:27 +0100

    Saving settings now work.
    It works end-to-end but the whole code needs to be cleaned up:
     - Naming of variable will never survive other settings.
     - Management of defaults in storage and UI should change.
     - The UI is unreadable.
     - Lack of automated save or similar.
     - Missing tests

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -10,6 +10,8 @@ export class MastSettings extends LitElement {
   // Currently known settings, incl. modification made through the UI.
   @state() private currentSettings?: pb.Settings;
 
+  @state() loadingBarUsers = 0;
+
   private defaultSettings?: pb.Settings;
 
   private inputRef: Ref<HTMLInputElement> = createRef();
@@ -30,21 +32,42 @@ export class MastSettings extends LitElement {
     if (!this.defaultSettings || !this.currentSettings) {
       throw new Error("missing settings");
     }
-
-    if (this.checkBoxRef.value) {
-      this.checkBoxRef.value.checked = false;
-    }
+    this.checkBoxRef.value!.checked = false;
+    this.updateSettings();
   }
 
   changeCheckbox() {
     if (!this.defaultSettings || !this.currentSettings) {
       throw new Error("missing settings");
     }
-    const checked = this.checkBoxRef.value?.checked;
-    if (checked) {
-      this.currentSettings.defaultListCount = this.defaultSettings?.defaultListCount;
-      this.inputRef.value!.value = this.currentSettings.defaultListCount.toString();
+    if (this.checkBoxRef.value?.checked) {
+      this.inputRef.value!.value = this.defaultSettings.defaultListCount.toString();
     }
+    this.updateSettings();
+  }
+
+  updateSettings() {
+    if (!this.defaultSettings || !this.currentSettings) {
+      throw new Error("missing settings");
+    }
+
+    if (this.checkBoxRef.value?.checked) {
+      this.currentSettings.defaultListCount = BigInt(0);
+    } else {
+      this.currentSettings.defaultListCount = BigInt(this.inputRef.value!.value);
+    }
+    this.requestUpdate();
+  }
+
+  async save() {
+    if (!this.defaultSettings || !this.currentSettings) {
+      throw new Error("missing settings");
+    }
+
+    this.loadingBarUsers++;
+    console.log("settings", this.currentSettings);
+    await common.backend.updateSettings(this.currentSettings);
+    this.loadingBarUsers--;
   }
 
   render() {
@@ -52,11 +75,11 @@ export class MastSettings extends LitElement {
       throw new Error("missing settings");
     }
 
-    const isDefault = this.currentSettings.defaultListCount === BigInt(0);
+    const isDefault = this.currentSettings.defaultListCount == BigInt(0);
     const actualValue = isDefault ? this.defaultSettings.defaultListCount : this.currentSettings.defaultListCount;
 
     return html`
-      <mast-main-view selectedView="settings">
+      <mast-main-view .loadingBarUsers=${this.loadingBarUsers} selectedView="settings">
         <span slot="header">Settings</span>
         <div slot="list">
           <div>
@@ -67,7 +90,7 @@ export class MastSettings extends LitElement {
           </div>
         </div>
         <div slot="footer" class="centered">
-          <button>Save</button>
+          <button @click=${this.save}>Save</button>
         </div>
       </mast-main-view>
     `;
